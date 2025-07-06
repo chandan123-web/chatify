52ada2b416e529fb3f043d0967a91b14bbbd0b31
chandan123-web <ckumarram444@gmail.com>
2025-07-06 21:53:44 +0530

    changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT;
 
-const __dirname = path.resolve();
+//const __dirname = path.resolve();
 
 app.use(
   cors({
@@ -29,13 +29,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+//if (process.env.NODE_ENV === "production") {
+//  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+//  app.get("*", (req, res) => {
+//    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//  });
+//}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
